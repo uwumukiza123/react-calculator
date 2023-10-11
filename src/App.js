f1b2ebb506e00b6a4b622f2d936a3fb8aae1886a
uwumukiza123 <uwumukizalaetitia@gmail.com>
2023-10-11 17:14:11 +0200

    add react calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-eval */
+/* eslint-disable eqeqeq */
+import React from "react";
 
 function App() {
+  const [inputs, setInputs] = React.useState("");
+  const [display, setDisplay] = React.useState("");
+  const [operation, setOperation] = React.useState("");
+
+  const handleClick = (text) => {
+    console.log({ text });
+    if (text == "+/-") {
+      //
+    } else if (text == "%") {
+      const result = display / 100;
+      setDisplay(result);
+    } else if (text == "AC") {
+      setInputs("");
+      setDisplay("");
+    } else if (text == "x") {
+      setInputs(display);
+      setOperation("*");
+      setDisplay(text);
+    } else if (text == "÷") {
+      setInputs(display);
+      setOperation("/");
+      setDisplay(text);
+    } else if (text == "+" || text == "-") {
+      setInputs(display);
+      setOperation(text);
+      setDisplay(text);
+    } else if (text == "=") {
+      console.log({ inputs, operation, display });
+      const result = eval(inputs + operation + display);
+      setDisplay(result || "");
+    } else {
+      if (
+        display == "%" ||
+        display == "0" ||
+        display == "+" ||
+        display == "-" ||
+        display == "÷" ||
+        display == "x"
+      ) {
+        setDisplay("");
+      }
+      setDisplay((state) => state + text);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">
+        <div className="displaySection">{display}</div>
+        <div>
+          <div className="buttonRow">
+            {["AC", "+/-", "%", "÷"].map((item, index) => (
+              <div
+                key={index}
+                className={`btn ${item == "÷" && "ops"}`}
+                onClick={() => handleClick(item)}
+              >
+                {item}
+              </div>
+            ))}
+          </div>
+          <div className="buttonRow">
+            {["7", "8", "9", "x"].map((item, index) => (
+              <div
+                key={index}
+                className={`btn ${item == "x" && "ops"}`}
+                onClick={() => handleClick(item)}
+              >
+                {item}
+              </div>
+            ))}
+          </div>
+          <div className="buttonRow">
+            {["4", "5", "6", "-"].map((item, index) => (
+              <div
+                key={index}
+                className={`btn ${item == "-" && "ops"}`}
+                onClick={() => handleClick(item)}
+              >
+                {item}
+              </div>
+            ))}
+          </div>
+          <div className="buttonRow">
+            {["1", "2", "3", "+"].map((item, index) => (
+              <div
+                key={index}
+                className={`btn ${item == "+" && "ops"}`}
+                onClick={() => handleClick(item)}
+              >
+                {item}
+              </div>
+            ))}
+          </div>
+          <div className="buttonRow">
+            {["0", ".", "="].map((item, index) => (
+              <div
+                key={index}
+                className={`btn ${item == 0 && "zero"} ${item == "=" && "ops"}`}
+                onClick={() => handleClick(item)}
+              >
+                {item}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
